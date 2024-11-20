f86ab4eb8b6b90d7bcc4c17d8a6a11bced6bc19e
Douglas Sacramento <159919262+DouglasSacramento@users.noreply.github.com>
2024-11-20 14:24:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ let valorParcelas = document.querySelector("#valor-parcelas");
 const valorIpva = document.querySelector("#valor-ipva");
 const valorSeguro = document.querySelector("#valor-seguro");
 const calcTotal = document.querySelector("#button-calcTotal");
+
 const textTotal = document.querySelector("h2");
 const ipvaMensal = document.querySelector("#ipva-mensal");
 const seguroMensal = document.querySelector("#seguro-mensal");
-
-console.log(typeof valorFipe.value);
-/* valorFipe.value = 26000;
-valorParcelas.value = 769.9; */
+const parcelaMensal = document.querySelector("#parcela-mensal");
 
 calcTotal.addEventListener("click", (event) => {
   event.preventDefault();
@@ -28,12 +26,24 @@ calcTotal.addEventListener("click", (event) => {
     valorParcelas.classList.remove("alertImput");
   }
 
+  calculaIpva();
+  calculaSeguro();
+  calculoGeralText();
+
+  destaqueResultado();
+});
+
+function calculaIpva() {
   const IPVA = valorFipe.value * 0.04;
   valorIpva.value = IPVA.toFixed([2]);
+}
 
+function calculaSeguro() {
   const SEGURO = valorFipe.value * 0.06;
   valorSeguro.value = SEGURO.toFixed([2]);
+}
 
+function calculoGeralText() {
   let parcela = Number(valorParcelas.value);
   let ipva = Number(valorIpva.value);
   let seguro = Number(valorSeguro.value);
@@ -43,7 +53,15 @@ calcTotal.addEventListener("click", (event) => {
 
   const totalMes = parcela + ipva + seguro;
 
-  textTotal.textContent = `Total mensal: R$ ${totalMes.toFixed([2])}`;
+  parcelaMensal.textContent = `Parcela mensal: R$ ${parcela.toFixed([2])}`;
   ipvaMensal.textContent = `IPVA mensal: R$ ${ipva.toFixed([2])}`;
   seguroMensal.textContent = `Seguro mensal: R$ ${seguro.toFixed([2])}`;
-});
+  textTotal.textContent = `Total mensal: R$ ${totalMes.toFixed([2])}`;
+}
+
+function destaqueResultado() {
+  const labelIpva = document.querySelector("#label-ipva");
+  const labelSeguro = document.querySelector("#label-seguro");
+  labelIpva.style.color = "black";
+  labelSeguro.style.color = "black";
+}
